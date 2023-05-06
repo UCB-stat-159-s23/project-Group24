c4ede3684d5ec0d0886217339a60b807ffcdd480
gina0000 <gina_zhang@berkeley.edu>
2023-05-06 01:32:41 +0000

    add function mobility_trends_by_category

--- tools/utils.py
+++ tools/utils.py
@@ -24,3 +24,26 @@ def mobility_trends_by_year_month(data, category, plot_title):
     plt.ylabel('percent change from baseline')
     figname = category+'_by_year_month.png'
     plt.savefig('figures/'+figname);
+
+# define a function to visualize the mobility trends by category
+def mobility_trends_by_category(data, category, plot_title):
+    states = data['state'].dropna().unique()
+    state_index = 0
+    fig, ax = plt.subplots(nrows=11, ncols=5, figsize=(20,20))
+
+    for row in range(11):
+        for col in range(5):
+            try:
+                state_data = data[data['state'] == states[state_index]]
+                sns.lineplot(data=state_data, x='year_month',y=category, errorbar=None, ax=ax[row,col])
+                ax[row,col].axhline(y=0, linestyle='dashed', color='red')
+                ax[row,col].set_xticks([])
+                ax[row,col].set_xlabel('')
+                ax[row,col].set_ylabel('')
+                ax[row,col].set_title(states[state_index])
+                state_index = state_index + 1
+            except IndexError:
+                pass
+    fig.suptitle('{title} mobility trends by state'.format(title=plot_title), fontsize=24, fontweight='bold')
+    figname = category+'_mobility_trends_by_state.png' 
+    plt.savefig('figures/'+figname)
